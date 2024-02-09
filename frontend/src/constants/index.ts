bb4b375aaeb62adb4df9b66ad3d46ef29bdc7e91
pulak-sarmah <pulaksarmah333@gmail.com>
2024-02-09 16:06:08 +0530

    Add new files and update existing files

---
+++ frontend/src/constants/index.ts
@@ -0,0 +1,17 @@
+export const brandStats = [
+  {
+    number: "500",
+    title: "Brands Owners",
+    id: 1,
+  },
+  {
+    number: "2K",
+    title: "Active Users",
+    id: 2,
+  },
+  {
+    number: "50+",
+    title: "Partners",
+    id: 3,
+  },
+];
--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,8 +1,55 @@
+import { TbCircleLetterI } from "react-icons/tb";
+import { Link } from "react-router-dom";
+import "./home.scss";
+import { brandStats } from "../../constants";
+import heroImage from "../../assets/inv-img.png";
+
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-      <p>Welcome to the home page!</p>
+    <div className="home">
+      <nav className="container --flex-between">
+        <div className="logo">
+          <TbCircleLetterI size={35} />
+        </div>
+        <ul className="home-links">
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+          <li>
+            <button className="--btn --btn-primary">
+              <Link to="/login">Login</Link>
+            </button>
+          </li>
+
+          <li>
+            <button className="--btn --btn-primary">
+              <Link to="/dashboard">Dashboard</Link>
+            </button>
+          </li>
+        </ul>
+      </nav>
+
+      <section className="container hero --mt5">
+        <div className="hero-text">
+          <h2>Inventory & Stock management Solution</h2>
+          <p>
+            A simple and easy to use inventory management solution for small and
+            medium businesses.
+          </p>
+          <div className="--flex-start">
+            {brandStats.map((stat) => (
+              <div key={stat.id} className="--mr">
+                <h3 className="--color-white">{stat.number}</h3>
+                <p className="--color-white">{stat.title}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className="hero-image">
+          <img src={heroImage} alt="inventory" />
+        </div>
+      </section>
     </div>
   );
 };
