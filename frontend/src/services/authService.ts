a40c46d334f57a5df14f2e5c749ab1fb1479c5b2
pulak-sarmah <pulaksarmah333@gmail.com>
2024-02-10 20:26:10 +0530

    Refactor authService.ts to use a generic handleRequest function

--- frontend/src/services/authService.ts
+++ frontend/src/services/authService.ts
@@ -1,29 +1,31 @@
-import axios, { isAxiosError } from "axios";
-import { RegisterFormData } from "../types/types";
+import axios, { AxiosResponse, Method } from "axios";
+import { LoginFormData, RegisterFormData } from "../types/types";
 import { toast } from "react-toastify";
 
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL as string;
 
-export const registerUser = async (userData: RegisterFormData) => {
+const handleRequest = async (
+  method: Method,
+  url: string,
+  data: RegisterFormData | LoginFormData | null = null,
+  params: object = {},
+  successMessage: string = "Request successful"
+): Promise<AxiosResponse["data"] | void> => {
   try {
-    const response = await axios.post(
-      `${API_BASE_URL}/api/v1/users/register`,
-      userData,
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-        withCredentials: true,
-      }
-    );
+    const response = await axios({
+      method,
+      url: `${API_BASE_URL}${url}`,
+      data,
+      params,
+    });
 
     if (response.status >= 200 && response.status < 300) {
-      toast.success("User registered successfully");
+      toast.success(successMessage);
     }
 
     return response.data;
   } catch (error) {
-    if (isAxiosError(error)) {
+    if (axios.isAxiosError(error)) {
       const message =
         (error.response &&
           error.response.data &&
@@ -32,7 +34,25 @@ export const registerUser = async (userData: RegisterFormData) => {
         error.toString();
       toast.error(message);
     } else {
-      throw new Error("Something went wrong");
+      toast.error("Something went wrong");
     }
   }
 };
+
+export const registerUser = (userData: RegisterFormData) =>
+  handleRequest(
+    "post",
+    "/api/v1/users/register",
+    userData,
+    {},
+    "User registered successfully"
+  );
+
+export const loginUser = (userData: LoginFormData) =>
+  handleRequest(
+    "post",
+    "/api/v1/users/login",
+    userData,
+    {},
+    "User logged in successfully"
+  );
