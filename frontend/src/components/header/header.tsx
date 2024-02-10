a56d855f0f17dc628ccbfac78f434feee5002abe
pulak-sarmah <pulaksarmah333@gmail.com>
2024-02-10 23:11:48 +0530

    Update ESLint configuration and header component

--- frontend/src/components/header/header.tsx
+++ frontend/src/components/header/header.tsx
@@ -3,13 +3,18 @@ import "./header.scss";
 const Header = () => {
   return (
     <div className=" header header_content">
-      <div className=" --flex-between">
+      <div
+        className=" --flex-between"
+        style={{ display: "flex", alignItems: "center" }}
+      >
         <h3>
           <span className="--fw-thin ">Welcome,</span>
           <span className="--color-danger "> test</span>
         </h3>
 
-        <button className="--btn --btn-danger">LogOut</button>
+        <button style={{ marginBottom: "1rem" }} className="--btn --btn-danger">
+          LogOut
+        </button>
       </div>
       <hr />
     </div>
