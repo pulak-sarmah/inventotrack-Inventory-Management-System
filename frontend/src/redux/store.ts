57dda59c578e0478c9a9e13dd7ae9e13ea82ba79
pulak-sarmah <pulaksarmah333@gmail.com>
2024-02-11 18:49:39 +0530

    Update import path for authSlice

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "./features/authSlice";
+import authReducer from "./features/auth/authSlice";
 
 export const store = configureStore({
   reducer: {
