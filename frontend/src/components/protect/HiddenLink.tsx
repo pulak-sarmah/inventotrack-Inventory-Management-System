fec7d59dd758604ba7eee1d22123b861edc33cbd
pulak-sarmah <pulaksarmah333@gmail.com>
2024-02-11 14:32:07 +0530

    Add HiddenLink component for conditional rendering based on login status

---
+++ frontend/src/components/protect/HiddenLink.tsx
@@ -0,0 +1,21 @@
+import { useSelector } from "react-redux";
+import { selectIsLoggedIn } from "../../redux/features/authSlice";
+import { ReactNode } from "react";
+
+export const ShowOnLogin = ({ children }: { children: ReactNode }) => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
+  if (isLoggedIn) {
+    return { children };
+  }
+  return null;
+};
+
+export const ShowOnLogOut = ({ children }: { children: ReactNode }) => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
+  if (!isLoggedIn) {
+    return { children };
+  }
+  return null;
+};
