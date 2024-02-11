a3a0b139c423bdbed2a8d69f73655eb0db5ed2bd
pulak-sarmah <pulaksarmah333@gmail.com>
2024-02-11 19:15:02 +0530

    Refactor handleRequest function into separate file

--- frontend/src/services/authService.ts
+++ frontend/src/services/authService.ts
@@ -1,55 +1,10 @@
-import axios, { AxiosResponse, Method } from "axios";
 import {
   LoginFormData,
   RegisterFormData,
   forgotPasswordFormData,
   resetPasswordFormData,
 } from "../types/types";
-import { toast } from "react-toastify";
-
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL as string;
-
-const handleRequest = async (
-  method: Method,
-  url: string,
-  data:
-    | RegisterFormData
-    | LoginFormData
-    | forgotPasswordFormData
-    | resetPasswordFormData
-    | null = null,
-  params: object = {},
-  successMessage?: string
-): Promise<AxiosResponse["data"] | void> => {
-  try {
-    const response = await axios({
-      method,
-      url: `${API_BASE_URL}${url}`,
-      data,
-      params,
-    });
-
-    if (successMessage) {
-      if (response.status >= 200 && response.status < 300) {
-        toast.success(successMessage);
-      }
-    }
-
-    return response.data;
-  } catch (error) {
-    if (axios.isAxiosError(error)) {
-      const message =
-        (error.response &&
-          error.response.data &&
-          error.response.data.message) ||
-        error.message ||
-        error.toString();
-      toast.error(message);
-    } else {
-      toast.error("Something went wrong");
-    }
-  }
-};
+import { handleRequest } from "./requestHandler";
 
 export const registerUser = async (userData: RegisterFormData) => {
   const response = await handleRequest(
---
+++ frontend/src/services/requestHandler.ts
@@ -0,0 +1,52 @@
+import axios, { Method, AxiosResponse } from "axios";
+import { toast } from "react-toastify";
+import {
+  RegisterFormData,
+  LoginFormData,
+  forgotPasswordFormData,
+  resetPasswordFormData,
+} from "../types/types";
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL as string;
+
+export const handleRequest = async (
+  method: Method,
+  url: string,
+  data:
+    | RegisterFormData
+    | LoginFormData
+    | forgotPasswordFormData
+    | resetPasswordFormData
+    | null = null,
+  params: object = {},
+  successMessage?: string
+): Promise<AxiosResponse["data"] | void> => {
+  try {
+    const response = await axios({
+      method,
+      url: `${API_BASE_URL}${url}`,
+      data,
+      params,
+    });
+
+    if (successMessage) {
+      if (response.status >= 200 && response.status < 300) {
+        toast.success(successMessage);
+      }
+    }
+
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      toast.error(message);
+    } else {
+      toast.error("Something went wrong");
+    }
+  }
+};
