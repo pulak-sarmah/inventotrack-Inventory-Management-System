05acd93057a4c32a89cfbc2210773c690c36c4eb
pulak-sarmah <pulaksarmah333@gmail.com>
2024-02-11 18:48:46 +0530

    Add useRedirectLogInUser and useRedirectLogOutUser hooks

---
+++ frontend/src/hooks/useRedirectLogOutUser.ts
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+
+import { useNavigate } from "react-router-dom";
+import { SET_LOGIN } from "../redux/features/auth/authSlice";
+import { GetLoginStatus } from "../services/authService";
+import { toast } from "react-toastify";
+import { useDispatch } from "react-redux";
+
+const useRedirectLogOutUser = (path: string) => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    (async () => {
+      const { data: isLoggedIn } = await GetLoginStatus();
+      dispatch(SET_LOGIN(isLoggedIn));
+
+      if (!isLoggedIn) {
+        toast.error("Session expired, please login again.");
+        navigate(path);
+        return;
+      }
+    })();
+  }, [dispatch, navigate, path]);
+};
+
+export default useRedirectLogOutUser;
