caf551107e697ed359495e736aaeb135bcbc94bc
pulak-sarmah <pulaksarmah333@gmail.com>
2024-02-13 18:02:03 +0530

    Add rate limiting middleware to API routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,6 +5,7 @@ import errorHandler from "./middleware/errorHandler.middleware";
 import { asyncHandler } from "./utils/asyncHandler";
 import { ApiResponse } from "./utils/ApiResponse";
 import { corsOptions } from "./middleware/cors.middleware";
+import rateLimit from "express-rate-limit";
 
 const app = express();
 
@@ -15,6 +16,14 @@ app.use(express.urlencoded({ extended: true, limit: "32kb" }));
 app.use(cors(corsOptions));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+//rate limiter
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 150,
+});
+
+app.use("/api", limiter);
 
 //import routes
 import userRouter from "./routes/user.route";
@@ -28,7 +37,7 @@ app.use("/api/v1/contact-us", contactRouter);
 
 //status check
 app.get(
-  "/api/v1/status",
+  "/",
   asyncHandler(async (_, res) => {
     res.status(200).json(new ApiResponse(200, true, "Server is running"));
   })
