db088a2a6dd92d22b98f31203cd50e8672add852
pulak-sarmah <pulaksarmah333@gmail.com>
2024-02-13 22:27:12 +0530

    Add getProducts functionality to productSlice.ts

--- frontend/src/redux/features/product/productSlice.ts
+++ frontend/src/redux/features/product/productSlice.ts
@@ -64,6 +64,36 @@ export const createProduct = createAsyncThunk<
   }
 });
 
+//get all products using createAsyncThunk
+export const getProducts = createAsyncThunk<
+  IProduct[],
+  void,
+  { rejectValue: RejectedValue }
+>("products/getAll", async (_, thunkAPI) => {
+  try {
+    const response = await productService.getProducts();
+    if (response.status < 200 || response.status >= 300) {
+      throw new Error("product upload failed");
+    }
+    return response;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      toast.error(message);
+      return thunkAPI.rejectWithValue({ message });
+    } else {
+      return thunkAPI.rejectWithValue({
+        message: "Something went wrong while adding product",
+      });
+    }
+  }
+});
+
 const productSlice = createSlice({
   name: "product",
   initialState,
@@ -81,16 +111,35 @@ const productSlice = createSlice({
         createProduct.fulfilled,
         (state, action: { payload: IProduct }) => {
           state.isLoading = false;
-          console.log(state.isLoading, "is loading from added");
           state.isSuccess = true;
+          state.isError = false;
           state.products.push(action.payload);
         }
       )
       .addCase(createProduct.rejected, (state, action) => {
         state.isLoading = false;
-        console.log(state.isLoading, "is loading from error");
         state.isError = true;
         state.message = action.payload?.message || "Something went wrong";
+      })
+      .addCase(getProducts.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(
+        getProducts.fulfilled,
+        (state, action: { payload: IProduct[] }) => {
+          state.isLoading = false;
+          state.isSuccess = true;
+          state.isError = false;
+          state.products = action.payload;
+          console.log(action.payload);
+          state.products = action.payload;
+        }
+      )
+      .addCase(getProducts.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload?.message || "Something went wrong";
+        console.log(action.payload, "message");
       });
   },
 });
--- frontend/src/services/productService.ts
+++ frontend/src/services/productService.ts
@@ -1,6 +1,7 @@
 import { ProductData } from "../types/types";
 import { handleRequest } from "./requestHandler";
 
+//create product
 const createProduct = async (productData: ProductData) => {
   const response = await handleRequest(
     "post",
@@ -13,8 +14,34 @@ const createProduct = async (productData: ProductData) => {
   return response;
 };
 
+//get all products
+const getProducts = async () => {
+  const response = await handleRequest(
+    "get",
+    "/api/v1/products/get-products",
+    null,
+    {}
+  );
+
+  return response;
+};
+
+//get product
+const getProduct = async (id: string) => {
+  const response = await handleRequest(
+    "get",
+    `/api/v1/products/get-product/${id}`,
+    null,
+    {}
+  );
+
+  return response;
+};
+
 const productService = {
   createProduct,
+  getProducts,
+  getProduct,
 };
 
 export default productService;
