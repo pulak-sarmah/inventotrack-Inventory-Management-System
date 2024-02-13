0318b251e3c3f62f72a6f78f190b6e30ceed23e9
pulak-sarmah <pulaksarmah333@gmail.com>
2024-02-13 16:14:15 +0530

    Update bcrypt import in users.model.ts

--- backend/src/models/users.model.ts
+++ backend/src/models/users.model.ts
@@ -1,5 +1,5 @@
 import mongoose, { Document } from "mongoose";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 export interface IUser extends Document {
