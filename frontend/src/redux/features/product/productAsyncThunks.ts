a12ad79f7de7c2c2b012142f7401c07bd30fbcfe
pulak-sarmah <pulaksarmah333@gmail.com>
2024-02-16 13:35:26 +0530

    Add product-related async thunks

---
+++ frontend/src/redux/features/product/productAsyncThunks.ts
@@ -0,0 +1,125 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import productService from "../../../services/productService";
+import { IProduct, ProductData } from "../../../types/types";
+
+interface RejectedValue {
+  message: string;
+}
+
+//create new product using createAsyncThunk
+export const createProduct = createAsyncThunk<
+  IProduct,
+  any,
+  { rejectValue: RejectedValue }
+>("products/create", async (formData: ProductData, thunkAPI) => {
+  try {
+    const response = await productService.createProduct(formData);
+
+    if (response.status < 200 || response.status >= 300) {
+      throw new Error("product upload failed");
+    }
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      return thunkAPI.rejectWithValue({ message });
+    } else {
+      return thunkAPI.rejectWithValue({
+        message: "Something went wrong while adding product",
+      });
+    }
+  }
+});
+
+//get all products using createAsyncThunk
+export const getProducts = createAsyncThunk<
+  IProduct[],
+  void,
+  { rejectValue: RejectedValue }
+>("products/getAll", async (_, thunkAPI) => {
+  try {
+    const response = await productService.getProducts();
+    if (response.status < 200 || response.status >= 300) {
+      throw new Error("product upload failed");
+    }
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      return thunkAPI.rejectWithValue({ message });
+    } else {
+      return thunkAPI.rejectWithValue({
+        message: "Something went wrong while fetching product",
+      });
+    }
+  }
+});
+
+// delete a product
+export const deleteProduct = createAsyncThunk<
+  null,
+  string,
+  { rejectValue: RejectedValue }
+>("product/delete", async (id: string, thunkAPI) => {
+  try {
+    const response = await productService.deleteProduct(id);
+    if (response.status < 200 || response.status >= 300) {
+      throw new Error("product could not be deleted");
+    }
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      return thunkAPI.rejectWithValue({ message });
+    } else {
+      return thunkAPI.rejectWithValue({
+        message: "Something went wrong while deleting product",
+      });
+    }
+  }
+});
+
+// get a product by id
+export const getProduct = createAsyncThunk<
+  IProduct,
+  string | undefined,
+  { rejectValue: RejectedValue }
+>("product/getById", async (id, thunkAPI) => {
+  try {
+    const response = await productService.getProduct(id);
+    if (response.status < 200 || response.status >= 300) {
+      throw new Error("product could not be found");
+    }
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      return thunkAPI.rejectWithValue({ message });
+    } else {
+      return thunkAPI.rejectWithValue({
+        message: "Something went wrong while fetching product",
+      });
+    }
+  }
+});
